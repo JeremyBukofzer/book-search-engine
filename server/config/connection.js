97edd3c1f4cebb8d192861fd99f28364f02250c8
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-02-08 18:58:01 -0600

    syntax changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
