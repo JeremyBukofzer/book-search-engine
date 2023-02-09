ea42a1dc4a6851ef8dd7ce8098f027529929bb28
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-02-08 20:23:57 -0600

    fixed funny typo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,7 +38,7 @@ const typeDefs = gql`
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        assUser(username: String!, email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook(bookData: BookInput!): User
         removeBook(bookId: ID!): User
     }
